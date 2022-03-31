292fd0796b683a3f5d21dd95e4b953bb6e1e0093
Sam <56883358+s-wdh@users.noreply.github.com>
2022-03-31 13:10:44 +0200

    new path recognition

--- PacMan/Script/Build/Script.js
+++ PacMan/Script/Build/Script.js
@@ -43,134 +43,65 @@ var Script;
     let viewport;
     let graph;
     let pacman;
+    let positionPacman;
     let grid;
-    let speed = new ƒ.Vector3(0, 0, 0);
-    let direction;
+    let speed = 1 / 60;
+    let direction = ƒ.Vector3.ZERO();
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
+        viewport.camera.mtxPivot.rotateY(180);
+        viewport.camera.mtxPivot.translateZ(-27);
+        viewport.camera.mtxPivot.translateX(-12);
+        viewport.camera.mtxPivot.translateY(7.5);
         graph = viewport.getBranch();
         pacman = graph.getChildrenByName("PacMan")[0];
         grid = graph.getChildrenByName("Grid")[0];
-        //console.log(pacman);
         //console.log(pacman.getComponent(ƒ.ComponentTransform));
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
+        positionPacman = pacman.mtxLocal.translation;
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-            speed.set(1 / 50, 0, 0);
-            direction = "right";
-            checkPath();
+            direction.set(1, 0, 0);
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-            speed.set(-1 / 50, 0, 0);
-            direction = "left";
-            checkPath();
+            direction.set(-1, 0, 0);
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-            speed.set(0, 1 / 50, 0);
-            direction = "up";
-            checkPath();
+            direction.set(0, 1, 0);
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-            speed.set(0, -1 / 50, 0);
-            direction = "down";
-            checkPath();
+            direction.set(0, -1, 0);
         }
-        pacman.mtxLocal.translate(speed);
+        checkPath();
         viewport.draw();
-        ƒ.AudioManager.default.update();
-    }
+        //ƒ.AudioManager.default.update();
+    } //update
     function checkPath() {
-        let position = pacman.mtxLocal.translation;
+        //check if next element is a path or wall
         let nextElementNumber;
-        let row;
         let nextElement;
-        switch (direction) {
-            case "left": {
-                //console.log(position.x, position.y);
-                nextElementNumber = Math.trunc(position.x) - 1;
-                if (nextElementNumber < 0) {
-                    speed.set(0, 0, 0);
-                    break;
-                }
-                console.log(nextElementNumber);
-                row = grid.getChild(Math.trunc(position.y));
-                nextElement = row.getChild(nextElementNumber);
-                let nextElementMesh = nextElement.getComponent(ƒ.ComponentMesh);
-                let nextElementColor = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-                console.log(nextElementColor);
-                if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-                    console.log("YAY");
-                }
-                else {
-                    speed.set(0, 0, 0);
-                }
-                break;
+        let rownr;
+        nextElementNumber = Math.trunc(positionPacman.x) - direction.x;
+        rownr = Math.trunc(positionPacman.y) - direction.y;
+        if (nextElementNumber < 1 || nextElementNumber > 23 || rownr < 1 || rownr > 13) {
+            direction.set(0, 0, 0);
+        }
+        else {
+            nextElement = grid.getChild(rownr).getChild(nextElementNumber);
+            let nextElementMesh = nextElement.getComponent(ƒ.ComponentMesh);
+            let nextElementColor = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
+            console.log(nextElementColor);
+            if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
+                pacman.mtxLocal.translate(ƒ.Vector3.SCALE(direction, speed));
             }
-            case "up": {
-                nextElementNumber = Math.trunc(position.x);
-                if (nextElementNumber > 14) {
-                    speed.set(0, 0, 0);
-                    break;
-                }
-                console.log(nextElementNumber);
-                row = grid.getChild(Math.trunc(position.y) + 1);
-                nextElement = row.getChild(nextElementNumber);
-                let nextElementMesh = nextElement.getComponent(ƒ.ComponentMesh);
-                let nextElementColor = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-                console.log(nextElementColor);
-                if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-                    console.log("YAY");
-                }
-                else {
-                    speed.set(0, 0, 0);
-                }
-                break;
-            }
-            case "right": {
-                nextElementNumber = Math.trunc(position.x) + 1;
-                if (nextElementNumber > 24) {
-                    speed.set(0, 0, 0);
-                    break;
-                }
-                console.log(nextElementNumber);
-                row = grid.getChild(Math.trunc(position.y));
-                nextElement = row.getChild(nextElementNumber);
-                let nextElementMesh = nextElement.getComponent(ƒ.ComponentMesh);
-                let nextElementColor = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-                console.log(nextElementColor);
-                if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-                    console.log("YAY");
-                }
-                else {
-                    speed.set(0, 0, 0);
-                }
-                break;
-            }
-            case "down": {
-                nextElementNumber = Math.trunc(position.x);
-                if (nextElementNumber < 0) {
-                    speed.set(0, 0, 0);
-                    break;
-                }
-                console.log(nextElementNumber);
-                row = grid.getChild(Math.trunc(position.y) - 1);
-                nextElement = row.getChild(nextElementNumber);
-                let nextElementMesh = nextElement.getComponent(ƒ.ComponentMesh);
-                let nextElementColor = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-                console.log(nextElementColor);
-                if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-                    console.log("YAY");
-                }
-                else {
-                    speed.set(0, 0, 0);
-                }
-                break;
+            else {
+                direction.set(0, 0, 0);
             }
         }
-    }
-})(Script || (Script = {}));
+    } //checkpath
+})(Script || (Script = {})); //namespace
 //# sourceMappingURL=Script.js.map
--- PacMan/Script/Source/Main.ts
+++ PacMan/Script/Source/Main.ts
@@ -5,17 +5,21 @@ namespace Script {
   let viewport: ƒ.Viewport;
   let graph: ƒ.Node;
   let pacman: ƒ.Node;
+  let positionPacman: ƒ.Vector3;
   let grid: ƒ.Node;
-  let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
-  let direction: string;
+  let speed: number = 1 / 60;
+  let direction: ƒ.Vector3 = ƒ.Vector3.ZERO();
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    viewport.camera.mtxPivot.rotateY(180);
+    viewport.camera.mtxPivot.translateZ(-27);
+    viewport.camera.mtxPivot.translateX(-12);
+    viewport.camera.mtxPivot.translateY(7.5);
     graph = viewport.getBranch();
     pacman = graph.getChildrenByName("PacMan")[0];
     grid = graph.getChildrenByName("Grid")[0];
-    //console.log(pacman);
 
     //console.log(pacman.getComponent(ƒ.ComponentTransform));
 
@@ -24,118 +28,46 @@ namespace Script {
   }
 
   function update(_event: Event): void {
-
     // ƒ.Physics.simulate();  // if physics is included and used
-
+    positionPacman = pacman.mtxLocal.translation;
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-      speed.set(1 / 50, 0, 0);
-      direction = "right";
-      checkPath();
+      direction.set(1, 0, 0);
     }
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-      speed.set(-1 / 50, 0, 0);
-      direction = "left";
-      checkPath();
+      direction.set(- 1, 0, 0);
     }
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-      speed.set(0, 1 / 50, 0);
-      direction = "up";
-      checkPath();
+      direction.set(0, 1, 0);
     }
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-      speed.set(0, -1 / 50, 0);
-      direction = "down";
-      checkPath();
+      direction.set(0, -1, 0);
     }
 
-    pacman.mtxLocal.translate(speed);
+    checkPath();
+
     viewport.draw();
-    ƒ.AudioManager.default.update();
-  }
+    //ƒ.AudioManager.default.update();
+  } //update
 
   function checkPath(): void {
-    let position: ƒ.Vector3 = pacman.mtxLocal.translation;
+    //check if next element is a path or wall
     let nextElementNumber: number;
-    let row: ƒ.Node;
     let nextElement: ƒ.Node;
-    switch (direction) {
-      case "left": {
-        //console.log(position.x, position.y);
-        nextElementNumber = Math.trunc(position.x) - 1;
-        if (nextElementNumber < 0) {
-          speed.set(0, 0, 0);
-          break;
-        }
-        console.log(nextElementNumber);
-        row = grid.getChild(Math.trunc(position.y));
-        nextElement = row.getChild(nextElementNumber);
-        let nextElementMesh: ƒ.Component = nextElement.getComponent(ƒ.ComponentMesh);
-        let nextElementColor: ƒ.Color = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-        console.log(nextElementColor);
-        if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-          console.log("YAY");
-        } else {
-          speed.set(0, 0, 0);
-        }
-        break;
-      }
-      case "up": {
-        nextElementNumber = Math.trunc(position.x);
-        if (nextElementNumber > 14) {
-          speed.set(0, 0, 0);
-          break;
-        }
-        console.log(nextElementNumber);
-        row = grid.getChild(Math.trunc(position.y) + 1);
-        nextElement = row.getChild(nextElementNumber);
-        let nextElementMesh: ƒ.Component = nextElement.getComponent(ƒ.ComponentMesh);
-        let nextElementColor: ƒ.Color = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-        console.log(nextElementColor);
-        if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-          console.log("YAY");
-        } else {
-          speed.set(0, 0, 0);
-        }
-        break;
-      }
-      case "right": {
-        nextElementNumber = Math.trunc(position.x) + 1;
-        if (nextElementNumber > 24) {
-          speed.set(0, 0, 0);
-          break;
-        }
-        console.log(nextElementNumber);
-        row = grid.getChild(Math.trunc(position.y));
-        nextElement = row.getChild(nextElementNumber);
-        let nextElementMesh: ƒ.Component = nextElement.getComponent(ƒ.ComponentMesh);
-        let nextElementColor: ƒ.Color = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-        console.log(nextElementColor);
-        if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-          console.log("YAY");
-        } else {
-          speed.set(0, 0, 0);
-        }
-        break;
-      }
-      case "down": {
-        nextElementNumber = Math.trunc(position.x);
-        if (nextElementNumber < 0) {
-          speed.set(0, 0, 0);
-          break;
-        }
-        console.log(nextElementNumber);
-        row = grid.getChild(Math.trunc(position.y) - 1);
-        nextElement = row.getChild(nextElementNumber);
-        let nextElementMesh: ƒ.Component = nextElement.getComponent(ƒ.ComponentMesh);
-        let nextElementColor: ƒ.Color = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
-        console.log(nextElementColor);
-        if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
-          console.log("YAY");
-        } else {
-          speed.set(0, 0, 0);
-        }
-        break;
+    let rownr: number;
+    nextElementNumber = Math.trunc(positionPacman.x) - direction.x;
+    rownr = Math.trunc(positionPacman.y) - direction.y;
+    if (nextElementNumber < 1 || nextElementNumber > 23 || rownr < 1 || rownr > 13) {
+      direction.set(0, 0, 0);
+    } else {
+      nextElement = grid.getChild(rownr).getChild(nextElementNumber);
+      let nextElementMesh: ƒ.Component = nextElement.getComponent(ƒ.ComponentMesh);
+      let nextElementColor: ƒ.Color = nextElementMesh.node.getComponent(ƒ.ComponentMaterial).clrPrimary;
+      console.log(nextElementColor);
+      if (nextElementColor.r == 1 && nextElementColor.g == 1 && nextElementColor.b == 1 && nextElementColor.a == 1) {
+        pacman.mtxLocal.translate(ƒ.Vector3.SCALE(direction, speed));
+      } else {
+        direction.set(0, 0, 0);
       }
     }
-  }
-}
+  } //checkpath
+} //namespace
